0432f40218f2bcdc8e32ebd2aa416da868623845
Sokritha Yen <sokritha17@gmail.com>
2021-02-09 05:41:08 +0700

    Filter out small object and Find the centroid of each object

--- main.py
+++ main.py
@@ -1,62 +1,100 @@
 import cv2
-from skimage import io, morphology
-from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import imutils
 
 
-# Reading image
-img = cv2.imread("image1.jpg", 1)
-
-original_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-#Converting image to LAB Color so CLAHE can be applied to the luminance channel
-lab_img= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
-
-#Splitting the LAB image to L, A and B channels, respectively
-l, a, b = cv2.split(lab_img)
-
-###########Histogram Equlization#############
-#Apply histogram equalization to the L channel
-equ = cv2.equalizeHist(l)
-
-#Combine the Hist. equalized L-channel back with A and B channels
-updated_lab_img1 = cv2.merge((equ,a,b))
-
-#Convert LAB image back to color (RGB)
-hist_eq_img = cv2.cvtColor(updated_lab_img1, cv2.COLOR_LAB2BGR)
-
-###########CLAHE#########################
-#Apply CLAHE to L channel
-clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
-clahe_img = clahe.apply(l)
-
-#Combine the CLAHE enhanced L-channel back with A and B channels
-updated_lab_img2 = cv2.merge((clahe_img,a,b))
-
-#Convert LAB image back to color (RGB)
-CLAHE_img = cv2.cvtColor(updated_lab_img2, cv2.COLOR_LAB2BGR)
-gray = cv2.cvtColor(CLAHE_img, cv2.COLOR_BGR2GRAY)
+def contrastLimit(img):
+    # Converting image to LAB Color so CLAHE can be applied to the luminance channel
+    lab_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    # Splitting the LAB image to L, A and B channels, respectively
+    l, a, b = cv2.split(lab_img)
+    # Apply histogram equalization to the L channel
+    equ = cv2.equalizeHist(l)
+    # Combine the Hist. equalized L-channel back with A and B channels
+    updated_lab_img = cv2.merge((equ, a, b))
+    # Convert LAB image back to color (RGB)
+    hist_eq_img = cv2.cvtColor(updated_lab_img, cv2.COLOR_LAB2BGR)
+    # Apply CLAHE to L channel
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    clahe_img = clahe.apply(l)
+    # Combine the CLAHE enhanced L-channel back with A and B channels
+    updated_lab_img2 = cv2.merge((clahe_img, a, b))
+    # Convert LAB image back to color (RGB)
+    CLAHE_IMG = cv2.cvtColor(updated_lab_img2, cv2.COLOR_LAB2BGR)
+    return CLAHE_IMG
 
 
-# Laplacian Original Image
-ori_laplacian_img = cv2.Laplacian(original_grey, cv2.CV_64F)
-# cv2.imshow("Edge Laplacian Original image", ori_laplacian_img)
-
-# Laplacian CLAHE Image
-clahe_laplacian_img = cv2.Laplacian(gray, cv2.CV_64F)
-# cv2.imshow("Edge Laplacian CLAHE image", clahe_laplacian_img)
-
-#Canny Original Image
-ori_edges = cv2.Canny(original_grey, 100, 200)
-# cv2.imshow("Edge Canny Original image", ori_edges)
-
-#Canny CLAHE
-clahe_edges = cv2.Canny(gray, 200, 200)
-cv2.imshow('Edge Canny CLAHE image', clahe_edges)
+def readImage(file):
+    return cv2.imread(file, 1)
 
 
-# cv2.imshow("Original image", img)
-# cv2.imshow("Equalized image", hist_eq_img)
-cv2.imshow('Gray original Image', original_grey)
-# cv2.imshow('Gray CLAHE Image', gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def canny(gray_img):
+    return cv2.Canny(gray_img, 200, 200)
+
+
+def removeSmallComponents(image, threshold):
+    # find all your connected components (white blobs in your image)
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(image, connectivity=8)
+    sizes = stats[1:, -1];
+    nb_components = nb_components - 1
+
+    img2 = np.zeros((output.shape), dtype=np.uint8)
+    # for every component in the image, you keep it only if it's above threshold
+    for i in range(0, nb_components):
+        if sizes[i] >= threshold:
+            img2[output == i + 1] = 255
+    return img2
+
+
+def resizeImg(img):
+    return cv2.resize(img, (480, 480))
+
+
+def preprocess_img(img):
+    image = contrastLimit(img)
+    image = canny(image)
+    return image
+
+
+def findContour(image):
+    # find contours in the thresholded image
+    return cv2.findContours(image.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+
+def findCenterObject(contours, image):
+    cnts = imutils.grab_contours(contours)
+    for c in cnts:
+        # compute the center of the contour
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+
+#         # draw the contour and center of the shape on the image
+#         cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+#         cv2.circle(image, (cX, cY), 7, (255, 255, 255), -1)
+#         cv2.putText(image, "center", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+#         # show the image
+#         cv2.imshow("Image", image)
+
+
+def main():
+    original_img = readImage('image9.jpg')
+    resized_img = resizeImg(original_img)
+    preprocessing_img = preprocess_img(resized_img)
+    filter_small_comp = removeSmallComponents(preprocessing_img, 150)
+    contour_img = findContour(filter_small_comp)
+    findCenterObject(contour_img, resized_img)
+    #     cv2.drawContours(resized_img, contour_img, -1, (0, 255, 0), 2)
+    #     cv2.imshow('Contours Image', resized_img)
+
+    #     cv2.imshow('Original Image', original_img)
+    #     cv2.imshow('Resized Image', resized_img)
+    #     cv2.imshow('Preprocessing Image', preprocessing_img)
+    #     cv2.imshow('Filter Out Small Component On Image', filter_small_comp)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+main()
